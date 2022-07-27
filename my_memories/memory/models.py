5f00dc6f33484d45e4a41f056b7e16cb445eb831
guitarcode <choicco89@gmail.com>
2022-07-27 17:02:58 +0900

    jwt와 dj-rest-auth 를 사용한 custom registeration 구현

---
+++ my_memories/memory/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.utils import timezone
+from django.conf import settings
+
+class Memory(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='memories', on_delete=models.CASCADE)
+    date = models.DateTimeField(default=timezone.now)
+    body = models.CharField(max_length=200)
+
+# Create your models here.
